e840f9e3e309954ebc465ed3e1a9ad052a185d9d
Fomenko Vladislav <sibest0110@yandex.ru>
2023-09-27 20:46:06 +0300

    Попытка выполнить метод сервиса

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -5,5 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class DataHandlerService {
 
+  organisationName: string = 'microsoft';
+  page: number = 1;
+  repoPerPage:number = 10;
+  url:string = `https://api.github.com/orgs/microsoft/repos?per_page=${this.repoPerPage}&page=${this.page}`;
+
   constructor() { }
+
+  testPing(): void{
+    alert("PING DATA-HANDLER");
+    console.log("PING DATA-HANDLER");
+    
+  }
 }
--- src/app/views/main-table/main-table.component.ts
+++ src/app/views/main-table/main-table.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataHandlerService } from 'src/app/services/data-handler.service';
 
 @Component({
   selector: 'app-main-table',
   templateUrl: './main-table.component.html',
   styleUrls: ['./main-table.component.css']
 })
-export class MainTableComponent {
+export class MainTableComponent implements OnInit {
+  constructor(private dataHandler: DataHandlerService){
 
+  }
+
+  ngOnInit(): void {
+    this.dataHandler.testPing();
+  }
 }
