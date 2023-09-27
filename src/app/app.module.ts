8841ae68071216bf8f8dc6f2ca6058480a8de1ac
Fomenko Vladislav <sibest0110@yandex.ru>
2023-09-27 20:20:49 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainTableComponent } from './views/main-table/main-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTableComponent,
   ],
   imports: [
     BrowserModule
