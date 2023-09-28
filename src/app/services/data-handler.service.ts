39119c1063811b9e19605010cabac403bb79ab83
Fomenko Vladislav <sibest0110@yandex.ru>
2023-09-28 19:51:41 +0300

    (feature) универсальный запрос репо

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -2,14 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RepoRow } from '../model/repo-row';
 
+export enum repoSortedBy {created='created', updated='updated', pushed='pushed', full_name='full_name'};
+export enum repoSortedDirection {asc='asc', desc='desc'};
+export enum repoType {all='all', public='public', private='private', forks='forks', sources='sources', member='member'};
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
   
+
   constructor(private httpClient: HttpClient) { }
 
-  getRepos(url:string, repoPerPage?:number, page?:number): RepoRow[]{
+  // getRepos(url:string, repoPerPage?:number, page?:number, sortedBy?:sortedBy, sortedDirection?:sortedDirection, repoType?:repoType): RepoRow[]{
+  getRepos({
+    url='https://api.github.com/orgs/microsoft/repos',
+    repoPerPage=undefined,
+    page=undefined,
+    sortedBy=undefined,
+    sortedDirection=undefined,
+    type=undefined
+  }:{
+    url:string,
+    repoPerPage?:number,
+    page?:number,
+    sortedBy?:repoSortedBy,
+    sortedDirection?:repoSortedDirection,
+    type?:repoType
+  }): RepoRow[]{
     let result: RepoRow[] = [];
     
     //#region Формирование url
@@ -21,6 +41,16 @@ export class DataHandlerService {
     if (page) {
       urlArgs.push(`page=${page}`);
     }
+    if (sortedBy) {
+      urlArgs.push(`sort=${sortedBy}`);
+    }
+    if (sortedDirection) {
+      urlArgs.push(`direction=${sortedDirection}`);
+    }
+    if (type) {
+      urlArgs.push(`type=${type}`);
+    }
+
     urlTail = urlArgs.join("&");
     if (urlTail){
       url += `?${urlTail}`;
