18d834c2e27cdc94466964e5e80dacf78ee22663
sibest0110 <sibest0110@yandex.ru>
2023-10-09 20:32:25 +0300

    test prism

---
+++ src/app/views/prism-test/prism-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prism-test',
+  templateUrl: './prism-test.component.html',
+  styleUrls: ['./prism-test.component.css']
+})
+export class PrismTestComponent {
+
+}
