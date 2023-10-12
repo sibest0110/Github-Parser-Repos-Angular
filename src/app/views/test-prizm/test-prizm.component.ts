9d00b016e951b1899b3cac773075958aa142a8da
sibest0110 <sibest0110@yandex.ru>
2023-10-12 21:25:29 +0300

    к человеческому виду призму

--- src/app/views/test-prizm/test-prizm.component.ts
+++ src/app/views/test-prizm/test-prizm.component.ts
@@ -15,14 +15,17 @@ export class TestPrizmComponent {
 constructor(private dataHandler: DataHandlerService, private testSubject: TestSubjectService){}
 
 
-  test(){
-    alert('кнопка нажата2');
-
+  async loadDefaultRepos(){
+    this.repoData = await this.dataHandler.getRepos({ url: "https://api.github.com/orgs/microsoft/repos", repoPerPage: 100, page: 1});
+    this.testSubject.changeData(this.repoData);
+    return ;
   }
 
-  async loadRepos() {
-    alert('кнопка нажата');
-    this.repoData = await this.dataHandler.getRepos({ url: "https://api.github.com/orgs/microsoft/repos", repoPerPage: 4, page: 8});
+  async loadRandomRepos() {
+    let randomPerPage = Math.floor(Math.random()*10) + 1;
+    let randomPage = Math.floor(Math.random()*10) + 1;
+    // alert(`Страница ${randomPage}\nЭлементов ${randomPerPage}`);
+    this.repoData = await this.dataHandler.getRepos({ url: "https://api.github.com/orgs/microsoft/repos", repoPerPage: randomPerPage, page: randomPage});
     this.testSubject.changeData(this.repoData);
     return ;
   }
